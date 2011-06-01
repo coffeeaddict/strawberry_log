c2295841279f7fe2f29819661688a8fb127d92f2
Hartog C. de Mik <hcdm@matchvertise.com>
2011-06-01 11:22:47 +0200

    First release

---
+++ lib/strawberry_log.rb
@@ -0,0 +1,23 @@
+module ActiveSupport
+  # log errors in red
+  class BufferedLogger
+    alias_method :orig_error, :error
+    def error msg
+      if Rails.env != :production
+        msg = "\e[1;31m#{msg}\e[0m"
+      end
+
+      orig_error msg
+    end
+
+    alias_method :orig_debug, :debug
+    def debug msg
+      if Rails.env != :production
+        msg = "\e[3;33m#{msg}\e[0m"
+      end
+
+      orig_debug msg
+    end
+
+  end
+end
