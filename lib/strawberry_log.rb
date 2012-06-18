444b7af1495a24a3bd87defb230e6268773f0fef
Hartog C. de Mik <hartog@organisedminds.com>
2012-06-18 13:19:44 +0200

    Method definition with braces

--- lib/strawberry_log.rb
+++ lib/strawberry_log.rb
@@ -2,7 +2,7 @@ module ActiveSupport
   # log errors in red
   class BufferedLogger
     alias_method :orig_error, :error
-    def error msg
+    def error(msg)
       if Rails.env != :production
         msg = "\e[1;31m#{msg}\e[0m"
       end
@@ -11,7 +11,7 @@ module ActiveSupport
     end
 
     alias_method :orig_debug, :debug
-    def debug msg
+    def debug(msg)
       if Rails.env != :production
         msg = "\e[3;33m#{msg}\e[0m"
       end
